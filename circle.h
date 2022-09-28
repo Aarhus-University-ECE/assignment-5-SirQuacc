f25dbd9b57d99291162ed34643f0b197b40db0c1
SirQuacc <steffentp@protonmail.ch>
2022-09-28 17:57:06 +0200

    test4

--- circle.h
+++ circle.h
@@ -3,7 +3,10 @@ typedef struct point {
 	int x;
 	int y;
 } point;
-
+typedef struct circle {
+	point p;
+	int r;
+} c
 /*add your definition of circle here. You need top call it "circle"*/
 
 void fiveCircles(circle c[]);
